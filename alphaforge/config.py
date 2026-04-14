ae01f034ffa5ee9a232798c6d550a3cf9c2c3aae
alimukri5-create <alimukri5@gmail.com>
2026-04-14 13:42:43 +0500

    Add AlphaForge configuration

---
+++ alphaforge/config.py
@@ -0,0 +1,12 @@
+"""Small central configuration values for the AlphaForge MVP."""
+
+from __future__ import annotations
+
+DEFAULT_TICKER = "MSFT"
+DEFAULT_PERIOD = "1y"
+SUPPORTED_PERIODS = ["6mo", "1y", "2y", "5y"]
+TRADING_DAYS_PER_YEAR = 252
+ATR_WINDOW = 14
+SUPPORT_LOOKBACK = 20
+RESISTANCE_LOOKBACK = 20
+BREAKOUT_BUFFER = 0.5
